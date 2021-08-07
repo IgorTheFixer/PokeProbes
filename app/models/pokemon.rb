02496e5b0577c76586c068f369a58e49b3fee74d
IgorTheFixer <cjeanphilippe95@gmail.com>
2021-08-07 11:53:57 -0400

    added Pokemon Model

---
+++ app/models/pokemon.rb
@@ -0,0 +1,7 @@
+class Pokemon < ApplicationRecord
+    validates :name, presence: true
+    validates :body, presence: true
+    
+    has_many :reviews
+    mount_uploader :photo_path, PhotoUploader
+  end
