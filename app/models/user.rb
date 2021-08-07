5e9c5baa3d7509dde84846ed00f54bc37c39a0d6
IgorTheFixer <cjeanphilippe95@gmail.com>
2021-08-07 11:56:47 -0400

    Added profile photos to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  mount_uploader :profile_photo, PhotoUploader
+
+  validates :username, uniqueness: true, presence: true
+
+  has_many :reviews
+  has_many :votes
+
+  def admin?
+    role == "admin"
+  end
 end
