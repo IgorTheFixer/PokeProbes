abad81fd87f8cb4d997ae4bac8c6fa04d917af92
IgorTheFixer <cjeanphilippe95@gmail.com>
2021-08-09 01:31:33 -0400

    initial work on user experience

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  root 'homes#index'
+  root 'static_pages#index'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :pokemons, only: [:new, :create]
+
+  get '/pokemons', to: 'static_pages#index'
+  get '/pokemons/:id', to: 'static_pages#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :pokemons, only: [:index, :show] do
+        resources :reviews, only: [:create, :destroy] do
+          resources :votes, only: [:create]
+        end
+      end
+    end
+  end
 end
